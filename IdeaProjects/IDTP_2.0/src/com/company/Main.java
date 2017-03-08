3ed4dedf7005293cce7100534e3465ce99d38653
Chris Scites <cscites@gmail.com>
2017-03-08 11:54:40 -0500

    2017-03-08_work

--- IdeaProjects/IDTP_2.0/src/com/company/Main.java
+++ IdeaProjects/IDTP_2.0/src/com/company/Main.java
@@ -1,18 +1,10 @@
 package com.company;
 
-import com.gargoylesoftware.htmlunit.WebClient;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
 
 public class Main {
-    public static final WebClient PLINK = new WebClient();
 
+    public static void main(String[] args){
 
-    public static void main(String[] args) throws IOException, URISyntaxException {
-        //Login myLogin = new Login();
-
-        Login2 newLogin = new Login2();
-        //Candidate newCandidate = new Candidate();
     }
 }
