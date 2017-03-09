d37be85edf4591e188444f19fec9d2664e820156
Chris <cscites@gmail.com>
2017-03-08 23:49:58 -0500

    2017-03-08_home

---
+++ IdeaProjects/IDTP_2.0/src/com/company/Browser.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import com.gargoylesoftware.htmlunit.CookieManager;
+import com.gargoylesoftware.htmlunit.WebClient;
+
+/**
+ * Created by chris on 3/8/17.
+ */
+public class Browser {
+    public Browser(){
+        final WebClient BROWSER = new WebClient();
+        CookieManager cookieManager = new CookieManager();
+
+    }
+
+}
