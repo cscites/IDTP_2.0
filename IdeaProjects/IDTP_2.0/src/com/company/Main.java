b65eb97c6f35d7f869f6eac6bf84ab615ad685f8
Chris Scites <cscites@gmail.com>
2017-03-09 17:02:10 -0500

    2017-03-08_work

--- IdeaProjects/IDTP_2.0/src/com/company/Main.java
+++ IdeaProjects/IDTP_2.0/src/com/company/Main.java
@@ -1,10 +1,30 @@
 package com.company;
 
 
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args){
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        Browser browser = new Browser();
+        System.out.println("Enter a URL");
+        browser.setUrl(scanner.nextLine());
+
+
+        HtmlPage page = browser.getPage();
+
+        String asText = page.asText();
+
+
+        System.out.println(asText);
+
+
 
     }
 }
