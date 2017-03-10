9c586c97aa156c8a8ee6c398fcd7300048148d13
Chris <cscites@gmail.com>
2017-03-10 00:52:48 -0500

    2017-03-10_home

--- IdeaProjects/IDTP_2.0/src/com/company/Browser.java
+++ IdeaProjects/IDTP_2.0/src/com/company/Browser.java
@@ -1,16 +1,28 @@
 package com.company;
 
-import com.gargoylesoftware.htmlunit.CookieManager;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+import java.io.IOException;
 
 /**
  * Created by chris on 3/8/17.
  */
 public class Browser {
-    public Browser(){
-        final WebClient BROWSER = new WebClient();
-        CookieManager cookieManager = new CookieManager();
+    private final WebClient BROWSER;
 
+    public Browser(){
+        BROWSER = new WebClient();
+    }
+
+
+    public HtmlPage page(String url) throws IOException {
+        return BROWSER.getPage(url);
+    }
+
+
+    public HtmlPage currentPage() throws IOException {
+        return page(BROWSER.getCurrentWindow().getEnclosedPage().getUrl().toString());
     }
 
 }
--- IdeaProjects/IDTP_2.0/src/com/company/Main.java
+++ IdeaProjects/IDTP_2.0/src/com/company/Main.java
@@ -1,10 +1,31 @@
 package com.company;
 
 
+import com.gargoylesoftware.htmlunit.html.*;
+
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+        Browser browser = new Browser();
+        String url = "https://www.google.com";
+        HtmlPage page = browser.page(url);
+        System.out.println(page.asText());
+
+        HtmlElement search = page.getElementByName("q");
+        search.setAttribute("value", "Cats");
+        HtmlElement go = page.getElementByName("btnK");
+        go.click();
+        HtmlPage page1 = browser.currentPage();
+        System.out.println(page1.asText());
+
+
+
+
+
+
+
 
     }
 }
