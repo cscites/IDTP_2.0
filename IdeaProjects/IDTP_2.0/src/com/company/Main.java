8f359d9db399a8a6c5c4e9bbe1f28bb680761463
Chris Scites <cscites@gmail.com>
2017-03-10 16:47:22 -0500

    2017-03-10_work

--- IdeaProjects/IDTP_2.0/src/com/company/Main.java
+++ IdeaProjects/IDTP_2.0/src/com/company/Main.java
@@ -9,26 +9,5 @@ public class Main {
         Login login = new Login();
         Candidate candidate = new Candidate();
 
-
-
-
-/**        String url = "https://www.google.com";
-        HtmlPage page = browser.page(url);
-        System.out.println(page.asText());
-
-        HtmlElement search = page.getElementByName("q");
-        search.setAttribute("value", "Cats");
-        HtmlElement go = page.getElementByName("btnK");
-        go.click();
-        HtmlPage page1 = browser.currentPage();
-        System.out.println(page1.asText());
-*/
-
-
-
-
-
-
-
     }
 }
